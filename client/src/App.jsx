c97d4faf2a8f018cc4da1f223bb8f8655cf7e222
ayushi2112arora <ayushi18arora@gmail.com>
2024-07-17 21:30:16 +0530

    changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,42 @@
- const App = () => {
+// import React from "react";
+// import {BrowserRouter , Router, Switch, Route} from "react-router-dom";
+// import  home  from "./pages/home";
 
-  return <h1>our main root project</h1>
-};
+// function App  () {
+//    return (
+//       <BrowserRouter>
+//       <h1>heloo welcome to react pages</h1>
+//          <Switch>
+//             <Route path="/" element={<home />}></Route>
+//          </Switch>
+//       </BrowserRouter>
+   
+//    );
+// }
+// export default App;
 
+import { BrowserRouter as Router, Route, Link, Routes }
+    from "react-router-dom";
+
+// Import the pages
+import { React } from "react";
+//import { Home } from "./pages/Home";
+import { About } from "./pages/about.jsx";
+
+
+
+function App() {
+    return (
+        
+            <Router>
+                <Routes>
+                   {/* // <Route  path="/" element={<h1>Home Page</h1>} /> */}
+                    <Route  path="/about" element={<About></About>} />
+                    
+                </Routes>
+                
+            </Router>
+   
+    );
+}
 export default App;
---
+++ client/src/pages/about.jsx
@@ -0,0 +1,3 @@
+export const About = () =>{
+    return <h1>hello About page</h1>
+};
